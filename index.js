07411dce235345c91f7b86c32da2fc0cafb011e3
Mikhail Kirpichou <mishania261282@.ru>
2025-10-31 16:34:56 +0300

    refactor:add the function run

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-import os from "os";
-import path from "path";
-import readline from "readline";
-import fs from "fs/promises";
+import FileManager from "./FileManager.js";
 
-const args = process.argv.slice(2);
-let username = "Guest";
-for (const arg of args) {
-  if (arg.startsWith("--username=")) {
-    [, username] = arg.split("=");
-    break;
+async function run() {
+  try {
+    const args = process.argv.slice(2);
+    let username = "Guest";
+    for (const arg of args) {
+      if (arg.startsWith("--username=")) {
+        [, username] = arg.split("=");
+        break;
+      }
+    }
+
+    const fileManager = new FileManager(username);
+    await fileManager.start();
+  } catch (error) {
+    console.error("An unhandled error occurred:", error);
+    process.exit(1);
   }
 }
 
-// Инициализация
-let currentWorkingDirectory = os.homedir();
-const rootDirectory = path.parse(currentWorkingDirectory).root;
-
-// Используем шаблонные строки для приветствия
-console.log(`Welcome to the File Manager, ${username}!`);
-console.log(`You are currently in ${currentWorkingDirectory}`);
+run();
